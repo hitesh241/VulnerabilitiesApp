41609d6b4b639263ad38e3f8931c5aae5b5fc58c
heetkoradia <heetkoradia@gmail.com>
2024-07-12 11:48:05 +0000

    cleaned code

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# app.py
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from cve_search import load_cve_database, search_cves
--- cve_search.py
+++ cve_search.py
@@ -1,13 +1,11 @@
-# cve_search.py
-
 import pandas as pd
 from fuzzywuzzy import fuzz
 
 def load_cve_database(csv_file):
     try:
         df = pd.read_csv(csv_file)
-        if not all(col in df.columns for col in ['CVE ID', 'description', 'cpe23uri']):
-            raise ValueError("CSV file must contain 'cve_id', 'description', and 'cpe23uri' columns.")
+        if not all(col in df.columns for col in ['CVE ID', 'Description', 'cpe23Uri']):
+            raise ValueError("CSV file must contain 'CVE ID', 'Description', and 'cpe23Uri' columns.")
         return df
     except Exception as e:
         print(f"Error loading CSV file: {e}")
@@ -16,20 +14,20 @@ def load_cve_database(csv_file):
 def search_cves(df, vendor, product, version, threshold=80):
     if df is None:
         return []
-    
+
     search_term = f'cpe:2.3:a:{vendor}:{product}:{version}'
-    
+
     # Token-based approach to filter potential matches before fuzzy matching
     try:
-        potential_matches = df[df['cpe23uri'].str.contains(vendor) & df['cpe23uri'].str.contains(product) & df['cpe23uri'].str.contains(version)]
+        potential_matches = df[df['cpe23Uri'].str.contains(vendor) & df['cpe23Uri'].str.contains(product) & df['cpe23Uri'].str.contains(version)]
     except KeyError as e:
         print(f"Error: Missing column in the DataFrame - {e}")
         return []
-    
+
     matching_cves = []
-    
+
     for index, row in potential_matches.iterrows():
-        if fuzz.partial_ratio(search_term, row['cpe23uri']) >= threshold:
-            matching_cves.append(row['cve_id'])
-    
+        if fuzz.partial_ratio(search_term, row['cpe23Uri']) >= threshold:
+            matching_cves.append(row['CVE ID'])
+
     return matching_cves
